41971729cd1fcd309eb22d366fc51a4a114c797b
ydang <dang.187pm13897@vanlanguni.vn>
2021-12-07 13:04:22 +0700

    initialization

---
+++ FamilyHealCare.IntegationTest/IntegrationTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FamilyHealCare.IntegationTest
+{
+    public class IntegrationTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ FamilyHealthCare.UnitTest/UnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FamilyHealthCare.UnitTest
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
