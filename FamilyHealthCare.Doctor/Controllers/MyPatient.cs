9ae24c9387ab874c9bfce74afa95910dbbf97432
187PM06696 - THAI NHAT QUANG - T5 <quang.187pm06696@vanlanguni.vn>
2022-01-17 01:42:57 +0700

    add client for doctor

---
+++ FamilyHealthCare.Doctor/Controllers/MyPatient.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyHealthCare.Customer.Controllers
+{
+    public class MyPatient : Controller
+    {
+        public IActionResult MyPatients()
+        {
+            return View();
+        }
+        public IActionResult MyPatientProfile()
+        {
+            return View();
+        }
+    }
+}
---
+++ FamilyHealthCare.Doctor/Controllers/Page.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyHealthCare.Customer.Controllers
+{
+    public class Page : Controller
+    {
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult AbouttUs()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
