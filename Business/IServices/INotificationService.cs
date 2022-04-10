089a792207c50354c55919d72f5d5c26e4b41ce8
ttydang <lapluga03@gmail.com>
2022-04-10 15:06:35 +0700

    setting up signalr to new service: notification service

---
+++ Business/IServices/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Business.IServices
+{
+    public interface INotificationService
+    {
+        public Task<IActionResult> CreateNotification();
+        public Task<IActionResult> MarkNotificationAsRead();
+    }
+}
---
+++ Business/Services/NotificationService.cs
@@ -0,0 +1,33 @@
+﻿using Business.IServices;
+using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using NotificationService.Hubs;
+using System;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class NotificationService : ControllerBase, INotificationService
+    {
+        private readonly IBaseRepository<Notification> _notificationRepository;
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public NotificationService(IBaseRepository<Notification> notificationRepository,
+            IHubContext<NotificationHub> hubContext)
+        {
+            _notificationRepository = notificationRepository;
+            _hubContext = hubContext;
+        }
+
+        public Task<IActionResult> CreateNotification()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> MarkNotificationAsRead()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
